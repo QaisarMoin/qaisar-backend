91017b546767ad2d08d35a4e041f79d01ac3a83c
QaisarMoin <qaisarashraf685@gmail.com>
2024-05-31 01:07:18 +0530

    controller file addition

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,4 @@
+// asyncHandler is high order function which means that it take function as a input and return a function as output
 const asyncHandler = (requesthandler) => {
   return (req, res, next) => {
     Promise.resolve(requesthandler(req, res, next)).catch((error) =>
@@ -5,3 +6,5 @@ const asyncHandler = (requesthandler) => {
     );
   };
 };
+
+export { asyncHandler };
